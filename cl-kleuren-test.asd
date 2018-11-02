ece02bd9c03bbf5ff32f90480a85a05ba2782248
Cole Lyman <cole@colelyman.com>
2018-11-01 19:15:48 -0600

    Added test cases for CdBG

--- cl-kleuren-test.asd
+++ cl-kleuren-test.asd
@@ -10,7 +10,8 @@
                "prove")
   :components ((:module "tests"
                 :components
-                ((:test-file "cl-kleuren"))))
+                ((:test-file "cl-kleuren")
+                 (:test-file "colored-de-bruijn-graph"))))
   :description "Test system for cl-kleuren"
 
   :perform (test-op (op c) (symbol-call :prove-asdf :run-test-system c)))
---
+++ tests/colored-de-bruijn-graph.lisp
@@ -0,0 +1,30 @@
+(defpackage colored-de-bruijn-graph-test
+  (:use :cl
+        :colored-de-bruijn-graph
+        :prove))
+(in-package :colored-de-bruijn-graph-test)
+
+(defparameter *k*
+  5)
+
+(defparameter *cdbg-test*
+  (make-instance '<colored-de-bruijn-graph> :k *k*))
+
+(defparameter *colors*
+  ())
+
+(plan 6)
+
+;; check accessor functions
+(is (k *cdbg-test*) *k*)
+(is (colors *cdbg-test*) nil)
+
+(build-genomes *cdbg-test* "../tests/data/small/genomes.txt")
+
+(is (get-node-colors *cdbg-test* "GCGGG") '(1 1 1 1))
+(is (get-node-colors *cdbg-test* "CACAT") '(1 0 0 0))
+(is (get-node-colors *cdbg-test* "CACTT") '(0 1 0 1))
+(is (get-node-colors *cdbg-test* "ACACA") '(2 1 1 1))
+
+
+(finalize)
